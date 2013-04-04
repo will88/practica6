6ce33d75d926b82a00bfd91c64eac4d93795f33f
alu3875 <diegoaguilar.88@gmail.com>
2013-04-04 12:19:26 +0100

    first commit

---
+++ ru_p6/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RuP6::Application.config.secret_token = '96fa2fc29e1678fece4a7a580e33b2d2bda85d7f1b6606ddbe554709cb2ed023a22ef449a119876738aa482e3851bc9c87206b9e66efbe29c7a0f9eb6a63b6c8'
